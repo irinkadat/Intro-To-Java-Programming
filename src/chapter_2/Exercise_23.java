8762c70a4f39f920ba16dee1c586a6434129c6e7
irinkadat <irina.datoshvili.1@btu.edu.ge>
2024-03-11 13:51:41 +0400

    add last exercise

---
+++ src/chapter_2/Exercise_23.java
@@ -0,0 +1,23 @@
+package chapter_2;
+import java.util.Scanner;
+
+public class Exercise_23 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the driving distance: ");
+        double distance = scanner.nextDouble();
+
+        System.out.print("Enter miles per gallon: ");
+        double milesPerGallon = scanner.nextDouble();
+
+        System.out.print("Enter price per gallon: ");
+        double pricePerGallon = scanner.nextDouble();
+
+        double costOfDriving = (distance / milesPerGallon) * pricePerGallon;
+
+        System.out.printf("The cost of driving is $%.2f%n", costOfDriving);
+
+        scanner.close();
+    }
+}
